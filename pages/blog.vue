f0a27a5e5f02f059126a234e0c8688cb7ed95e3e
Arapu Trevor <105975921+Trevor-143@users.noreply.github.com>
2023-07-11 11:38:56 +0300

    Add files via upload

---
+++ pages/blog.vue
@@ -0,0 +1,5 @@
+<template >
+    <div class="set" >
+        <h1>Blog</h1>
+    </div>
+</template>
---
+++ pages/contact.vue
@@ -0,0 +1,19 @@
+<template >
+    <div class="set" >
+        <h1>Contact</h1>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    .set {
+        background-color: #eee;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+    }
+</style>
